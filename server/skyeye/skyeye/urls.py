390d28b9a574aa1c1ded0bbb158872a30f6528ed
rc0r <hlt99@blinkenshell.org>
2016-10-02 13:40:44 +0200

    DATABASE.md added that specifies afl-utils database schemata

--- server/skyeye/skyeye/urls.py
+++ server/skyeye/skyeye/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^', include('aflutils.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^', include('aflutils.urls'))
 ]
