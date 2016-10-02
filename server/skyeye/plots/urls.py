6d717631d369d1b99791a18c3fe5f2e2e3165ee6
rc0r <hlt99@blinkenshell.org>
2016-10-02 12:46:05 +0200

    SkyEye views updated to use HTML templates. Template file for fuzzer stats overview added. Template file for detailed fuzzer plors added. Basic CSS stylesheet added.

--- server/skyeye/plots/urls.py
+++ server/skyeye/plots/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<fuzzer>\w+)/$', views.details, name='details')
 ]
